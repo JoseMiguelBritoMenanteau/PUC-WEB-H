1faa777c0c1e164cc9ee317f8c551abbc352ccf7
Gaspar Berner <gasparberner@gmail.com>
2025-10-14 20:13:06 -0300

    barra final

--- web-taumm/src/App.jsx
+++ web-taumm/src/App.jsx
@@ -2,44 +2,57 @@ import { useState } from 'react'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 
-import Navbar from './components/navbar/Navbar.jsx'
-import './App.css'
+
+import Navbar from './components/navbar/Navbar.jsx';
+import FinalBar from './components/finalbar/finalbar.jsx';
+import './App.css';
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: ( <>
-      <Navbar />
-    </>
-  ),
+    element: (
+      <>
+        <Navbar />
+        <FinalBar />
+      </>
+    ),
   },
   {
     path: '/login',
-    element: ( <>
-      <Navbar />
-    </>
-  ),
+    element: (
+      <>
+        <Navbar />
+        <FinalBar />
+      </>
+    ),
   },
   {
     path: '/productos',
-    element: ( <>
-      <Navbar />
-    </>
-  ),
+    element: (
+      <>
+        <Navbar />
+        <FinalBar />
+      </>
+    ),
   },
   {
     path: '/contacto',
-    element: ( <>
-      <Navbar />
-    </>
-  ),
+    element: (
+      <>
+        <Navbar />
+        <FinalBar />
+      </>
+    ),
   },
   {
     path: '/catalogo',
-    element: ( <>
-      <Navbar />
-    </>
-  ),
+    element: (
+      <>
+        <Navbar />
+        <FinalBar />
+      </>
+    ),
   },
 ]);
 
