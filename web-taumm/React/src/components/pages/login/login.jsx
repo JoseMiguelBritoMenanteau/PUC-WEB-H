c21eaa99ac5ffdb289dfe78ec37944d6bbb37092
JoseMiguelBritoMenanteau <jose.brito@estudiante.uc.cl>
2025-10-15 12:59:45 -0300

    Login Box Css Terminado

--- web-taumm/React/src/components/pages/login/login.jsx
+++ web-taumm/React/src/components/pages/login/login.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState, useRef, useEffect } from 'react';
 
 import './login.css'
+import { Link } from 'react-router';
 
 
 
@@ -13,14 +14,14 @@ const SignInBox = () => {
         <div className = 'sign-in-box-container'>
             <h2 className = 'sign-in-box-title'>Iniciar Sesión</h2>
             <form className="sign-in-form">
-                <input type="text" name="username" required  placeholder = 'Nombre de Usuario'/>
+                <input className = 'sign-in-input' type="text" name="username" required  placeholder = 'Nombre de Usuario'/>
 
-                <input type="password" name="password" required placeholder = 'Contraseña' />
+                <input className = 'sign-in-input' type="password" name="password" required placeholder = 'Contraseña' />
 
-                <button type="submit">Entrar</button>
+                <button type="submit" className = 'sign-in-button'>Entrar</button>
             </form>
             <div className = 'sign-in-box-footer'>
-
+                <Link to = '/register' className = 'sign-in-box-register-link'> ¿No tienes una cuenta? Contáctate </Link>
             </div>
         </div>
     )
