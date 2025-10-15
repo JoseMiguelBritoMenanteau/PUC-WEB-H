0501eb6c18515f5b833926b3b0e0a52b1e9d4e17
JoseMiguelBritoMenanteau <jose.brito@estudiante.uc.cl>
2025-10-15 12:28:29 -0300

    Login Avance

--- web-taumm/React/src/components/pages/login/login.jsx
+++ web-taumm/React/src/components/pages/login/login.jsx
@@ -13,9 +13,9 @@ const SignInBox = () => {
         <div className = 'sign-in-box-container'>
             <h2 className = 'sign-in-box-title'>Iniciar Sesión</h2>
             <form className="sign-in-form">
-                <input type="text" id="username" name="username" required  placeholder = 'Nombre de Usuario'/>
+                <input type="text" name="username" required  placeholder = 'Nombre de Usuario'/>
 
-                <input type="password" id="password" name="password" required placeholder = 'Contraseña' />
+                <input type="password" name="password" required placeholder = 'Contraseña' />
 
                 <button type="submit">Entrar</button>
             </form>
