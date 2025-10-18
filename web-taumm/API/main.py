e32fa962ed8c5361310873b15a975385da3c57ae
JoseMiguelBritoMenanteau <jose.brito@estudiante.uc.cl>
2025-10-18 17:12:27 -0300

    Final

--- web-taumm/API/main.py
+++ web-taumm/API/main.py
@@ -36,12 +36,13 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
-
+"""
 @app.get('/', status_code=status.HTTP_200_OK)
 async def user_root(user: user_dependency, db: db_dependency):
     if user is None:
         raise HTTPException(status_code = 401, detail = 'Autentificación Fallida')
     return {'User': user['username'], 'ID': user['id'], 'Authenticated': True}
+"""
 
 @app.get('/users', response_model = List[UserResponse])
 async def get_all_users(db: db_dependency):
